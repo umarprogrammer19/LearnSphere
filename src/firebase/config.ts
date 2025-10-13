03e0d0d81660bd0f50ef5d5654f46e9fabe18f8b
Umar <umarofficial0121@gmail.com>
2025-10-13 18:46:55 +0000

    Update Config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "apiKey": "AIzaSyBB184nw_es0XgxWMfLdbttdpOIiwwAvfo",
-  "authDomain": "education-system-uzass.firebaseapp.com",
-  "projectId": "education-system-uzass",
-  "storageBucket": "education-system-uzass.appspot.com",
-  "messagingSenderId": "722272209383",
-  "appId": "1:722272209383:web:911b5593808f1b4561d542",
-  "measurementId": "G-30NEZ7MPS4"
-};
+  apiKey: "AIzaSyBB184nw_es0XgxWMfLdbttdpOIiwwAvfo",
+  authDomain: "education-system-uzass.firebaseapp.com",
+  projectId: "education-system-uzass",
+  storageBucket: "education-system-uzass.firebasestorage.app",
+  messagingSenderId: "722272209383",
+  appId: "1:722272209383:web:911b5593808f1b4561d542",
+  measurementId: "G-30NEZ7MPS4"
+};;
