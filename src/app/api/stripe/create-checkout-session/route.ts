5b858075eeee2a280c089422c03d31d61665c081
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-10-16 02:40:20 +0500

    Secret Key Updated

--- src/app/api/stripe/create-checkout-session/route.ts
+++ src/app/api/stripe/create-checkout-session/route.ts
@@ -3,7 +3,7 @@
 import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY || '', {
   apiVersion: '2024-04-10',
 });
 
