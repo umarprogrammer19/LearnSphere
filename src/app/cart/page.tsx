3edaab9cf0486925df35245c13ee733f028ce85f
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-10-17 03:44:36 +0500

    Use Memo

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Header } from '@/components/header';
